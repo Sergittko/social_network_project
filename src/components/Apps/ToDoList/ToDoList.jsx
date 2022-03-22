c345bf2c1790bd0e689d61351762e66e86a7e71a
Sergittko <derov180214@gmail.com>
2022-03-22 02:01:44 +0200

    added toDoList design

--- src/components/Apps/ToDoList/ToDoList.jsx
+++ src/components/Apps/ToDoList/ToDoList.jsx
@@ -2,11 +2,37 @@ import React from "react";
 import style from "./ToDoList.module.css";
 
 let ToDoList = () => {
-  return(
+  return (
     <div className={style.todoList}>
-      ToDoList
+      <div className={style.tasks_list_global}>
+        <div className={style.tasks_list_wrapper}>
+          <h2 className={style.list_header}>Todo list</h2>
+          <div className={style.tasks_list}>
+            <ul className={style.ul_tasks}>
+              <li>
+                <span className={style.dot_span}/>
+                
+                <button type="button">
+                  +
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div className={style.input_tasks}>
+            <input
+              type="text"
+              id="input_task"
+              defaultValue=""
+              maxLength={30}
+              placeholder="type task there..."
+              autoComplete="off"
+            />
+            <button>Add task</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default ToDoList;
